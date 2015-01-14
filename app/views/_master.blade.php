95c7a7e37744128f47094c78cb94437f3b1d105e
Kate deBethune <kdebethune@gmail.com>
2015-01-14 12:17:33 -0500

    backing out analytics code - something broke

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -61,16 +61,6 @@
 
 <body data-spy="scroll" data-offset="0" data-target="#navbar-main">
 {{-- NOTE: CONTINUATION OF MENU CODE IS CALLED BY CHILD PAGES FROM global.php --}}
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-58565358-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 	
 	<div class="page-wrap">
 		@yield('content')
