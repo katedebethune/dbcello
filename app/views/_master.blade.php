c798be6a9e4391882af91a7b57bdcf62d0ab1941
Kate deBethune <kdebethune@gmail.com>
2016-06-05 17:20:04 -0400

    adding ga code

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -5,10 +5,10 @@
 
 	<title>@yield('title','Cello Lessons | Newton Brookline Wellesley &amp; Greater Boston | Suzuki Certified | Kate de Bethune Studio - cello instruction for all ages in Newton, MA')</title>
 	<meta charset='utf-8'>
-	<meta name="description" content="<meta property="og:title" content="Cello Lessons | Newton Brookline Wellesley &amp; Greater Boston | Suzuki Certified | Kate deBethune Studio - cello instruction for all ages in Newton, MA"/>
-<meta property="og:site_name" content="cello instruction for all ages in Newton, MA"/>
+	<!-- <meta name="description" content="<meta property="og:title" content="Cello Lessons | Newton Brookline Wellesley &amp; Greater Boston | Suzuki Certified | Kate deBethune Studio - cello instruction for all ages in Newton, MA"/>
+<meta property="og:site_name" content="cello instruction for all ages in Newton, MA"/> -->
 
-	<!-- BOOTSTRAP CORE INCLUDED WITH THE SHIELD THEME-->
+	<!-- BOOTSTRAP CORE INCLUDED WITH THE SHIELD THEME -->
     {{ HTML::style('assets/css/bootstrap.css') }}
 
 	<!-- Optional theme -->
@@ -72,7 +72,14 @@
 
 	@yield('footer')
 		{{ HTML::footer() }}
-	
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-58565358-1', 'auto');
+  ga('send', 'pageview');
+</script>	
 </body>
 
 </html>
